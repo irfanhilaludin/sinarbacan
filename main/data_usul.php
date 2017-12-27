7f4dc4fd8fb12bce6b86990a4b4a13109a88f6d0
Bayu Hafiz <bayuhafiz@mss.co.id>
2017-12-27 15:58:45 +0700

    updated

--- main/data_usul.php
+++ main/data_usul.php
@@ -24,70 +24,58 @@
     </tr>
     </thead>
     <tbody>
-    <!-----------------------------------Content------------------------------------>
-    <?php
+
+<?php
 
 // pilih admin area 
 if ($admin_area == 'all') {
-    $q = mysql_query("select * from tb_kontak_usul a LEFT JOIN tb_kontak_all b ON b.id_kontak = a.id_kontak LEFT JOIN (SELECT id_user, nama_user, kota from tb_user) c on c.id_user = a.id_user where a.status_kontak = 'menunggu'") or die (mysql_error());
+    $q = mysql_query("select * from tb_kontak_usul a LEFT JOIN (SELECT id_user, nama_user, kota from tb_user) b on b.id_user = a.id_user where a.status_kontak = 'menunggu'") or die (mysql_error());
 } else {
     switch ($admin_area) {
         case 'surabaya':
-            $area = "AND b.kota_kontak IN ('surabaya','tuban', 'sidoarjo', 'gersik', 'lamongan', 'mojokerto', 'jombang', 'nganjuk', 'madiun', 'ngawi', 'ponorogo', 'pacitan', 'trenggalek', 'tulungagung', 'blitar')";
+            $area = " AND a.kota_kontak IN ('surabaya','tuban', 'sidoarjo', 'gresik', 'lamongan', 'mojokerto', 'jombang', 'nganjuk', 'madiun', 'ngawi', 'ponorogo', 'pacitan', 'trenggalek', 'tulungagung', 'blitar')";
             break;
         case 'probolinggo':
-            $area = " AND b.kota_kontak IN ('probolinggo', 'pasuruan', 'batu', 'malang', 'lumajang', 'jember', 'bondowoso', 'situbondo', 'banyuwangi')";
+            $area = " AND a.kota_kontak IN ('probolinggo', 'pasuruan', 'batu', 'malang', 'lumajang', 'jember', 'bondowoso', 'situbondo', 'banyuwangi')";
             break;
         case 'semarang':
-            $area = " AND b.kota_kontak IN ('cilacap', 'banyumas', 'brebes', 'tegal', 'purbalingga', 'batang', 'banjarnegara', 'kebumen', 'wonosobo', 'purworejo', 'temanggung', 'magelang', 'kendal', 'yogyakarta', 'ungaran', 'salatiga', 'boyolali', 'klaten', 'surakarta', 'sukorejo', 'karanganyar', 'wonogiri', 'sragen', 'grobongan', 'demak', 'semarang')";
+            $area = " AND a.kota_kontak IN ('cilacap', 'banyumas', 'brebes', 'tegal', 'purbalingga', 'batang', 'banjarnegara', 'kebumen', 'wonosobo', 'purworejo', 'temanggung', 'magelang', 'kendal', 'yogyakarta', 'ungaran', 'salatiga', 'boyolali', 'klaten', 'surakarta', 'sukorejo', 'karanganyar', 'wonogiri', 'sragen', 'grobongan', 'demak', 'semarang')";
             break;
         case 'juwana' or 'juwono':
-            $area = " AND b.kota_kontak IN ('juwano', 'jepara', 'kudus', 'pati', 'rembang', 'blora')";
+            $area = " AND a.kota_kontak IN ('juwano', 'jepara', 'kudus', 'pati', 'rembang', 'blora')";
             break;
         case 'cirebon':
-            $area = " AND b.kota_kontak IN ('cirebon')";
+            $area = " AND a.kota_kontak IN ('cirebon')";
             break;
         default:
             $area = "";
             break;
     }
 
-    $q = mysql_query("select * from tb_kontak_usul a LEFT JOIN tb_kontak_all b ON b.id_kontak = a.id_kontak LEFT JOIN (SELECT id_user, nama_user, kota from tb_user) c on c.id_user = a.id_user where a.status_kontak = 'menunggu'" . $area) or die (mysql_error());
+    $q = mysql_query("select * from tb_kontak_usul a LEFT JOIN (SELECT id_user, nama_user, kota from tb_user) b on b.id_user = a.id_user where a.status_kontak = 'menunggu'" . $area) or die (mysql_error());
 }
    
-while ($row = mysql_fetch_array($q)) {;
-
-
-   
-	//$kontak_query = mysql_query("select * from tb_kontak_usul where status_kontak ='menunggu' AND pembuat <> 'admin'")or die(mysql_error());
-    //while($row = mysql_fetch_array($kontak_query)){
-	
-//$user_query = mysql_query("select * from tb_user where id_user = '$row[id_user]' ")or die(mysql_error());
-            //    $row_user = mysql_fetch_array($user_query);
-  
-
-    ?>
+while ($row = mysql_fetch_array($q)) {
+?>
     <tr>
-
-    <td><?php echo $row['nama_kontak']; ?></td>
-    <td><?php echo $row['kota_kontak']; ?></td>
-     <td><?php echo $row['provinsi_kontak']; ?></td>
-    <td><?php echo $row['telepon_kontak']; ?></td>
-    <td><?php echo $row['email_kontak']; ?></td>
-    <td><?php echo $row_user['nama_user']; ?></td>
-    <td><?php echo $row['status_kontak']; ?></td>  
+        <td><?php echo $row['nama_kontak']; ?></td>
+        <td><?php echo $row['kota_kontak']; ?></td>
+        <td><?php echo $row['provinsi_kontak']; ?></td>
+        <td><?php echo $row['telepon_kontak']; ?></td>
+        <td><?php echo $row['email_kontak']; ?></td>
+        <td><?php echo $row['nama_user']; ?> [<?php echo $row['kota']; ?>]</td>
+        <td><span class="label label-warning"><?php echo $row['status_kontak']; ?></span></td>  
         <td><?php echo $row['report_kontak']; ?></td>  
-    <td>
-    <a href="?page=filter&id_kontak=<?php echo $row['id_kontak']; ?>" class="btn btn-primary btn-xs" >Filter</a>
-    </td>		
+        <td>
+            <a href="?page=filter&id_kontak=<?php echo $row['id_kontak']; ?>" class="btn btn-primary btn-xs" >Filter</a>
+        </td>		
     </tr>
     
-    <?php 
-    
-    }?>  
+<?php 
+}
+?>  
     </tbody>
     </table>
-     </form>
-    
-    </div>
+    </form>
+</div>
     
